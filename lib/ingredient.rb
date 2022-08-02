92fb8a59693a3fac854afe6409fef25d872fb736
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2022-08-02 12:03:44 -0400

    test/feat: Ingredient test and class complete

--- lib/ingredient.rb
+++ lib/ingredient.rb
@@ -1,2 +1,17 @@
 class Ingredient
+  attr_reader  :details, :name, :unit, :calaries, :details
+  def initialize(details)
+    @details = details
+    @name = details[:name]
+    @unit = details[:unit]
+    @calaries = details[:calaries]
+  end
+
+    # def name
+    #   @details['name'] => 'name'
+    # end
+
+
+
+
 end
---
+++ spec/ingredient_spec.rb
@@ -0,0 +1,20 @@
+require './lib/ingredient'
+require './lib/pantry'
+
+RSpec.describe Ingredient do
+  let(:ingredient1) { Ingredient.new({name: "Cheese", unit: "oz", calories: 50}) }
+  let(:ingredient2) { Ingredient.new({name: "Macaroni", unit: "oz", calories: 200}) }
+
+  it "should exist as a class" do
+    expect(ingredient1).to be_a Ingredient
+  end
+
+  it 'should have a name for the item' do
+    expect(ingredient1.name).to eq ("Cheese")
+  end
+
+  it 'should should provide the units' do
+    expect(ingredient1.unit).to eq ('oz')
+  end
+
+end
